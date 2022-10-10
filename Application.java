5b8ae0e293f6994c575039caeab486a11d2acb17
Abdullah Al Abid <abdullahabid1660@gmail.com>
2022-10-10 09:45:46 +0600

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,26 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		short myShort = 827;
+		long myLong = 9797;
+		
+		double myDouble = 7.2345;
+		
+		char myChar = 'y';
+		boolean myBoolean =  false;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		
+	}
+
+}
