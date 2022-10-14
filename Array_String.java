11be18cf1530ba080a379d23a421a5965dc43e64
Abdullah Al Abid <abdullahabid1660@gmail.com>
2022-10-14 18:12:42 +0600

    Add files via upload

---
+++ Array_String.java
@@ -0,0 +1,29 @@
+
+public class Array_String {
+	public static void main(String[] args) {
+		String[] word = new String[3];
+		
+		word[0] = "Hello";
+		word[1] = "to";
+		word[2] = "you";
+		
+		System.out.println(word[2]);
+		
+		String[] fruits = {"Apple","Banana","pear","kiwi"};
+		
+		for(String fruit: fruits) {
+			System.out.println(fruit);
+		} 
+		
+		int value = 0;
+		
+		String text = null;
+		
+		System.out.println(text);
+		
+		String[] texts = new String[2];
+		
+		System.out.println(texts[0]);
+		
+	}
+}
