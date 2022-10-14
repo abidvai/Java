5d0ac8d0950f8d910a9b834700b6447b9a3580ee
Abdullah Al Abid <abdullahabid1660@gmail.com>
2022-10-14 09:02:54 +0600

    Add files via upload

---
+++ If_else.java
@@ -0,0 +1,17 @@
+
+public class If_else {
+	public static void main(String[] args) {
+		
+		int myInt = 5;
+		
+		if(myInt < 10) {
+			System.out.println("Yes, It's true");
+		}
+		else if(myInt < 20){
+			System.out.println("No, It's false!");
+		}
+		else {
+			System.out.println("None of the above.");
+		}
+	}
+}
