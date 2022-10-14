a226060d02c1b556120c4e8dcfeeabd672b76652
Abdullah Al Abid <abdullahabid1660@gmail.com>
2022-10-14 18:12:10 +0600

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Switch {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter a command: ");
+		String text = input.nextLine();
+		
+		switch(text) {
+		case "start":
+			System.out.println("Machine Started!");
+			break;
+			
+		case "stop":
+			System.out.println("Machine Stopped!");
+			break;
+			
+		default:
+			System.out.println("Command not found!!!");
+		}
+	}
+}
