f7c8ba25eb0023d03130479794d10b30f3a8069d
Abdullah Al Abid <abdullahabid1660@gmail.com>
2023-01-25 19:27:18 +0600

    Add files via upload

---
+++ Interfaces.java
@@ -0,0 +1,16 @@
+
+interface Animal{
+	void walk();
+}
+
+class Horse implements Animal{
+	 public void walk() {
+		System.out.println("walks on 4 legs");
+	}
+}
+
+public class Interfaces {
+	public static void main(String args[]) {
+		
+	}
+}
